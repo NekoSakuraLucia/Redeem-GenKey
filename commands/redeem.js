a137faa797e6d453e81de91760fc9c406d1a1f9e
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-05 14:57:22 +0700

    เพิ่มโค้ดในคำสั่ง redeem คีย์

--- commands/redeem.js
+++ commands/redeem.js
@@ -1,8 +1,28 @@
+const { dataKey } = require("../data");
+
 module.exports = {
     data: {
         name: "redeem",
     },
     execute: async (message, args) => {
-        message.channel.send("redeem")
+        if (args.length < 1) {
+            return message.reply("กรุณาระบุคีย์ที่ต้องการใช้!")
+        }
+
+        const redeemKey = args[0];
+
+        const keyEntry = dataKey.find(entry => entry.key === redeemKey);
+
+        if (!keyEntry) {
+            return message.reply("คีย์นี้ไม่ถูกต้อง!");
+        }
+
+        if (keyEntry.isUsed) {
+            return message.reply("คีย์นี้ถูกใช้งานไปแล้ว");
+        }
+
+        keyEntry.isUsed = true;
+
+        message.reply(`คุณได้ใช้คีย์: ${redeemKey} เรียบร้อยแล้ว!`)
     }
 };
