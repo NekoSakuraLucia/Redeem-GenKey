f76a9bc2b1c78b4230bc98398c1e0d44c05adc53
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-05 14:04:22 +0700

    เพิ่มการรับคำสั่ง แต่ละคำสั่งและจัดระเบียบโฟลเดอร์ให้สวยงาม

---
+++ commands/redeem.js
@@ -0,0 +1,8 @@
+module.exports = {
+    data: {
+        name: "redeem",
+    },
+    execute: async (message, args) => {
+        message.channel.send("redeem")
+    }
+};
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -10,8 +12,35 @@ const client = new Client({
     ]
 });
 
+client.commands = new Map();
+const commandFiles = fs.readdirSync(path.join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(path.join(__dirname, "commands", file));
+    client.commands.set(command.data.name, command);
+}
+
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 });
+
+client.on("messageCreate", async (message) => {
+    if (message.author.bot) return;
+
+    const args = message.content.split(" ");
+
+    const commandName = args[0].split(1).toLowerCase();
+
+    const command = client.commands.get(commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply("เกิดข้อผิดพลาดในการประมวลผลคำสั่ง!");
+    }
+})
 
 client.login(process.env.DISCORD_TOKEN ?? "");
